16b7d28465b02aaa28da5ef31bbc040a0f2c240b
TEPATEL <TEPATEL@TEPATEL-A40-D.in.oracle.com>
2022-05-20 02:17:16 +0530

    first commit

---
+++ src/main/java/com/tc/SpringBootEksApplication.java
@@ -0,0 +1,13 @@
+package com.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEksApplication.class, args);
+	}
+
+}
